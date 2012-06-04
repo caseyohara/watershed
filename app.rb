b6234da36523a7728c1bda850413e6518c9de847
Taylor Smith <mail@taylorsmith.net>
2012-06-03 18:43:19 -0600

    Add routes for index and favicon

--- app.rb
+++ app.rb
@@ -5,12 +5,17 @@ Bundler.require
 require './lib/parser'
 
 class App < Sinatra::Base
-  before do
-    content_type 'application/json'
-    @parser = Parser.new
+  get '/' do
+    ""
+  end
+
+  get '/favicon.ico' do
+    ""
   end
 
   get '/lake_elevation.json' do
+    content_type 'application/json'
+    @parser = Parser.new
     @parser.lake_elevation.to_json
   end
 end
